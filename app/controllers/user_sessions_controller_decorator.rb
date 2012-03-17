38538ec5bb88c2b14574bd9b0abfc6093c7cedc3
Paul Damer <pdamer@gmail.com>
2012-03-16 23:29:04 -0400

    fixing login failure on facebook setup

--- app/controllers/user_sessions_controller_decorator.rb
+++ app/controllers/user_sessions_controller_decorator.rb
@@ -1,8 +1,18 @@
 UserSessionsController.class_eval do
 
+  ssl_required :merge
+
   def merge
     # now sign in from the login form
     authenticate_user!
+    unless user_signed_in?
+      @user = User.find params[:user][:id]
+      @user.email = params[:user][:email]
+      
+      flash[:error] = I18n.t("devise.failure.invalid")
+      render(:template => 'users/merge')
+      return
+    end
 
     # prep for all the shifting and do it
     user = User.find(current_user.id)
@@ -18,7 +28,8 @@ UserSessionsController.class_eval do
 
     # tell the truth now
     flash[:alert] = "Succesfully linked your accounts"
-    sign_in_and_redirect(user, :event => :authentication)
+    sign_in(user, :event => :authentication)
+    redirect_back_or_default(root_path)
   end
 
 end
